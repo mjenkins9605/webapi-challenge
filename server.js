734a871b3e1f06066e69eeb548a859165105eef9
Michael Jenkins <mjenkins9605@vidangel.com>
2019-05-19 23:18:12 -0400

    added routers

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const projectRouter = require("./routers/projectRouters");
+const actionRouter = require("./routers/actionRouters");
 const server = express();
 
 server.use(express.json());
+server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
 
 server.get("/", (req, res) => {
   res.send(
